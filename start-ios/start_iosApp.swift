c036283b6a5410db8f1a2d0be3935816c6397e20
huydqdev <150894064+huydqdev@users.noreply.github.com>
2024-12-29 01:57:28 +0700

    Add files via upload
    initial change

---
+++ start-ios/start_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  start_iosApp.swift
+//  start-ios
+//
+//  Created by MACHOME on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct start_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
